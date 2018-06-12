6eefa1d3f057e333cdaae473caaea990ea617959
ThomasDenneulin <thomas.denneulin@devinci.fr>
2018-06-12 20:11:37 +0200

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+const TeleBot = require('telebot');
+
+
+const bot = new TeleBot('618996081:AAGY5hlbwhaE6UHWrrxTSSIu2m9cWpcOvBo');
+
+bot.on('/time', msg => {
+
+    return bot.sendMessage(msg.from.id, 'Getting time...').then(re => {
+        // Start updating message
+        updateTime(msg.from.id, re.message_id);
+    });
+
+});
+
+bot.on('text' , msg => {
+    fs.readFile('./config.json','utf8', (err,data) => {
+        var config = JSON.parse(data);
+        console.log(config);
+        if(config.indexOf(msg.from.id) == -1){
+            config.push(msg.from.id)
+            fs.writeFile('./config.json',JSON.stringify(config),(err)=>{
+                if(err) console.log(err)
+            })
+            return bot.sendMessage(msg.chat.id, `Bonjour ${msg.from.first_name} !`);
+            
+        }
+    })
+});
+bot.start();
